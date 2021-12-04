8211e11a4f16bfeb35025ef91d419478226136a1
sidoelz123 <sidoelz.123@gmail.com>
2021-12-04 12:46:24 +0700

    v 2.1

--- RESIK/pengguna/script.js
+++ RESIK/pengguna/script.js
@@ -7,16 +7,16 @@ closeBtn.addEventListener("click", ()=>{
   menuBtnChange();//calling the function(optional)
 });
 
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+searchBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
+  menuBtnChange(); 
 });
 
-// following are the code to change sidebar button(optional)
+
 function menuBtnChange() {
  if(sidebar.classList.contains("open")){
-   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
  }else {
-   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
  }
 }
